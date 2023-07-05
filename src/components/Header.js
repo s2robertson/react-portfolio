704ba97cd92ce4016f676140d9c8602fbb3d9d0f
Steven Robertson <s2robertson@gmail.com>
2023-07-05 19:48:43 -0400

    made header full-width

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 function Header({ children }) {
     return (
-        <header className="d-flex flex-column flex-lg-row align-items-center">
-            <h1 className="text-center text-lg-start flex-grow-1">Steven Robertson's Portfolio</h1>
-            {children}
+        <header className="bg-custom">
+            <div className="container p-3 d-flex flex-column flex-lg-row align-items-center">
+                <h1 className="text-center text-lg-start flex-grow-1">Steven Robertson's Portfolio</h1>
+                {children}
+            </div>
         </header>
     )
 }
