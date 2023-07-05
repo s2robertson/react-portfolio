b7139e87a2c6a71a44c190408fe85c5fab0be8fb
Steven Robertson <s2robertson@gmail.com>
2023-07-05 09:55:02 -0400

    adjusted header responsiveness

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,9 @@ import { PAGE_NAMES } from "./App";
 
 function Header({ activePage, onNavigate }) {
     return (
-        <header className="d-flex">
-            <h1 className="flex-grow-1">Steven Robertson's Portfolio</h1>
-            <nav>
+        <header className="d-flex flex-column flex-lg-row align-items-center">
+            <h1 className="text-center text-lg-start flex-grow-1">Steven Robertson's Portfolio</h1>
+            <nav className="d-flex flex-column flex-sm-row">
                 <a  href="#about"
                     className={activePage === PAGE_NAMES.ABOUT ? 'active btn' : 'btn'}
                     onClick={() => onNavigate(PAGE_NAMES.ABOUT)}
