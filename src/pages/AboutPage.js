2ccfc52ded53a5ca434b0bf0ce370279c5a554b7
Steven Robertson <s2robertson@gmail.com>
2023-07-04 20:12:42 -0400

    changed alt tag

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,7 +7,7 @@ function AboutPage() {
             <div>
                 <img
                     src={photo}
-                    alt='A photo of Steven'
+                    alt='Steven'
                     className='float-start mb-3 me-3 rounded-circle'
                 />
                 <p>I am a graduate of the software engineering program at University of Waterloo, where I worked on the full software development life cycle. I am a dedicated worker who likes to take ownership and make sure that a job isn't just done--it's done right. I enjoy both learning new skills and refining old ones, and am always looking for ways to grow.</p>
