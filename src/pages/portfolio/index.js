183618f79b68bc1d56541afdfbe598127f363962
Steven Robertson <s2robertson@gmail.com>
2023-07-05 15:42:12 -0400

    split Project off into its own component

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -46,19 +46,23 @@ function PortfolioPage() {
         <main>
             <h2>My Projects</h2>
             <div className='row g-3'>
-                {projects.map(project => 
-                    <div className='col-md-6' key={project.bgClass}>
-                        <div className={`project-list-item ${project.bgClass} p-3`}>
-                            <div className='bg-body p-3'>
-                                <h3>{project.title}</h3>
-                                <p>{project.description}</p>
-                                <p><a href={project.repoUrl}>Repo</a> <a href={project.deployedUrl}>Deployed</a></p>
-                            </div>
-                        </div>
-                    </div>
-                )}
+                {projects.map(project => (<Project project={project} className='col-md-6' key={project.bgClass} />))}
             </div>
         </main>
+    )
+}
+
+function Project({ project, className }) {
+    return (
+        <div className={className}>
+            <div className={`project-list-item ${project.bgClass} p-3`}>
+                <div className='bg-body p-3'>
+                    <h3>{project.title}</h3>
+                    <p>{project.description}</p>
+                    <p><a href={project.repoUrl}>Repo</a> <a href={project.deployedUrl}>Deployed</a></p>
+                </div>
+            </div>
+        </div>
     )
 }
 
