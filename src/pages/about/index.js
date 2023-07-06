2e221a463f75ad8d55c4f4e6803cf6f4b686793b
Steven Robertson <s2robertson@gmail.com>
2023-07-05 20:49:42 -0400

    repositioned photo, added more alt text

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -3,15 +3,13 @@ import photo from './steven-small.jpg';
 function AboutPage() {
     return (
         <>
+            <img
+                src={photo}
+                alt='Steven Robertson, a full-stack developer'
+                className='float-start mb-3 me-3 rounded-circle'
+            />
             <h2>About Me</h2>
-            <div>
-                <img
-                    src={photo}
-                    alt='Steven'
-                    className='float-start mb-3 me-3 rounded-circle'
-                />
-                <p>I am a graduate of the software engineering program at University of Waterloo, where I worked on the full software development life cycle. I am a dedicated worker who likes to take ownership and make sure that a job isn't just done--it's done right. I enjoy both learning new skills and refining old ones, and am always looking for ways to grow.</p>
-            </div>
+            <p>I am a graduate of the software engineering program at University of Waterloo, where I worked on the full software development life cycle. I am a dedicated worker who likes to take ownership and make sure that a job isn't just done--it's done right. I enjoy both learning new skills and refining old ones, and am always looking for ways to grow.</p>
         </>
     )
 }
